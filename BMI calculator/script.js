7ff6cbf79ca71f2474131ade75e315bc05133520
vlladd112 <vlladd112@gmail.com>
2018-09-18 14:22:14 +0300

    final touches were made

--- BMI calculator/script.js
+++ BMI calculator/script.js
@@ -1,6 +1,9 @@
-var person1 = document.getElementsByClassName("person1");
-var person2 = document.getElementsByClassName("person2");
-var claculate = document.getElementById("calc");
+let person1 = document.getElementsByClassName("person1");
+const results = document.getElementsByClassName("rezultat");
+const bmi = document.getElementsByClassName("bmi");
+const person2Checkbox = document.getElementById('person2Checkbox');
+const person2Parag = document.getElementById('person2Parag');
+const claculate = document.getElementById("calc");
 const weightStatus = document.getElementById('weightStatus');
 const normalBodyWeight = document.getElementById('normalBodyWeight');
 const weightPlusMinus = document.getElementById('weightPlusMinus');
@@ -10,74 +13,66 @@ const rezultatContainer = document.getElementsByClassName('rezultatContainer');
 const rezultate = document.getElementById('rezultate');
 const html = document.getElementById('html');
 
-function diferenta(x, y) {
-    var dif = x - y;
-    return dif;
-}
+//function diferenta(x, y) {
+//    let dif = x - y;
+//    return dif;
+//};
 function fbmi(h, kg) {
-    var imece = Math.ceil((kg / (h * h)) * 100);
-    var imece = imece / 100;
+    let imece = Math.ceil((kg / (h * h)) * 1000000);
+    imece = imece / 100;
     return imece;
-}
+};
 function diferenta(x,y) {
-    var dif = x - y;
+    let dif = x - y;
     dif = Math.round(dif * 10) / 10;
     return dif;
-}
+};
 function bmiNormal(h, kg) {
-    var normalbmi = Math.ceil((kg / (h * h)) * 100);
+    let normalbmi = Math.ceil((kg / (h * h)) * 1000000);
     normalbmi = normalbmi / 100;
     return normalbmi;
-}
+};
 function weightOfPerson(bmi, h) {
-    var weight = ((bmi * (h * h))  );
+    let weight = ((bmi * (h * h)) /10000 );
     weight = Math.round(weight * 10) / 10;
     return weight;
-}
+};
 function optimumWeight(normalWeightHigh, normalWeightLow) {
-    var optimumWeightForPerson = (normalWeightHigh + normalWeightLow) / 2;
+    let optimumWeightForPerson = (normalWeightHigh + normalWeightLow) / 2;
     optimumWeightForPerson = Math.round(optimumWeightForPerson * 10) / 10;
     return optimumWeightForPerson;
-}
+};
 function checkTypeL(person1) {
-    var letters = /^[A-Za-z-' ]+$/;
+    const letters = /^[A-Za-z-' ]+$/;
     if (person1.value.match(letters)) {
         person1.style.border = ""
         return true;
     } else {
         person1.style.border = "2px solid red";
     }
-}
-const checkValues = function checkValues (Yna, Yhe, Ywe) {
-    var letters = /^[A-Za-z]+$/;
-    if (Yna.value == "") {
-        Yna.style.border = "2px solid red";
+};
+const checkValues = function checkValues (name, height, weight) {
+    const letters = /^[A-Za-z]+$/;
+    if (name.value == "") {
+        name.style.border = "2px solid red";
     } else if (person1[0].value != "") {
-        Yna.style.border = "";
+        name.style.border = "";
     }
-    if (Yhe.value == "") {
-        Yhe.style.border = "2px solid red";
-    } else if (Yhe.value != "") {
-        Yhe.style.border = "";
+    if (height.value == "") {
+        height.style.border = "2px solid red";
+    } else if (height.value != "") {
+        height.style.border = "";
     }
-    if (Ywe.value == "") {
-        Ywe.style.border = "2px solid red";
-    } else if (Ywe.value != "") {
-        Ywe.style.border = "";
+    if (weight.value == "") {
+        weight.style.border = "2px solid red";
+    } else if (weight.value != "") {
+        weight.style.border = "";
     }
-    if (((Yna.value) && (Yhe.value) && (Ywe.value)) != "") {
+    if (((name.value) && (height.value) && (weight.value)) != "") {
         return true;
     }
-}
+};
 
-window.onload = function onload() {
-    person1 = document.getElementsByClassName("person1");
-    person2 = document.getElementsByClassName("person2");
-    results = document.getElementsByClassName("rezultat");
-    bmi = document.getElementsByClassName("bmi");
-    person2Checkbox = document.getElementById('person2Checkbox');
-    person2Parag = document.getElementById('person2Parag');
-}
 calc.onclick = ()=> {
     html.style.display = "flex";
     rezultate.style.display = "none";
@@ -89,13 +84,11 @@ calc.onclick = ()=> {
             html.style.display = "block";
             rezultate.style.display = "block";
             results[0].innerHTML = person1[0].value;
-            results[1].innerHTML = "Height: " + person1[1].value + " m";
+            results[1].innerHTML = "Height: " + (person1[1].value / 100) + " m";
             results[2].innerHTML = "Weight: " + person1[2].value + " kg";
             const bmiResultP1 = fbmi(person1[1].value, person1[2].value);
             console.log(bmiResultP1);
             bmi[0].innerHTML = bmiResultP1;
-//            normalBodyWeight.style.color = "green";
-//            optimumBodyWeight.style.color = "green";
             if(bmiResultP1 < 18.5) {
                 bmi[0].style.color = "dodgerblue";
                 weightStatus.style.color = "dodgerblue";
@@ -115,7 +108,7 @@ calc.onclick = ()=> {
                 const weightDifferenceOptimumPlus = diferenta(weightP1, optimumWeightPerson);
                 console.log(weightDifferenceOptimumPlus);
                 
-                normalBodyWeight.innerHTML = "Normal body weight is between " + "<span class='normalBodyWeightSpan'>" + weightNormalLow + "</span>" + " kg - " + "<span class='normalBodyWeightSpan'>" + weightNormalHigh + "</span>" + " kg";
+                normalBodyWeight.innerHTML = "Normal body weight: " + "<span class='normalBodyWeightSpan'>" + weightNormalLow + "</span>" + " kg - " + "<span class='normalBodyWeightSpan'>" + weightNormalHigh + "</span>" + " kg";
                 optimumBodyWeight.innerHTML = "Optimum weight: " + "<span id='optimumBodyWeightSpan'>" + optimumWeightPerson + "</span>" + " kg";
                 weightPlusMinus.innerHTML = "For normal weight, lose between " + weightDifferencePlusHigh + " kg - " + weightDifferencePlusLow + " kg";
                 optimumBodyWeightPlusMinus.innerHTML = "For optimum weight, lose " + "<span id='optimumBodyWeightPlusMinusSpan'>" + weightDifferenceOptimumPlus + "</span>" + " kg";
@@ -131,22 +124,22 @@ calc.onclick = ()=> {
                 bmi[0].style.color = "orange";
                 weightStatus.style.color = "orange";
                 optimumBodyWeightPlusMinusSpan.style.color = "orange";
-            } else if(bmiResultP1 < 35) {
-                weightStatus.innerHTML = "Obese (1st degree)";
-                bmi[0].style.color = "lightcoral";
-                weightStatus.style.color = "lightcoral";
-                optimumBodyWeightPlusMinusSpan.style.color = "lightcoral";
-            } else if(bmiResultP1 < 40) {
-                weightStatus.innerHTML = "Obese (2st degree)";
-                bmi[0].style.color = "red";
-                weightStatus.style.color = "red";
-                optimumBodyWeightPlusMinusSpan.style.color = "red";
-            } else if(bmiResultP1 > 40) {
-                weightStatus.innerHTML = "Morbid obesity";
-                bmi[0].style.color = "darkred";
-                weightStatus.style.color = "darkred";
-                optimumBodyWeightPlusMinusSpan.style.color = "darkred";
-            };
+                } else if(bmiResultP1 < 35) {
+                    weightStatus.innerHTML = "Obese (1st degree)";
+                    bmi[0].style.color = "lightcoral";
+                    weightStatus.style.color = "lightcoral";
+                    optimumBodyWeightPlusMinusSpan.style.color = "lightcoral";
+                } else if(bmiResultP1 < 40) {
+                    weightStatus.innerHTML = "Obese (2st degree)";
+                    bmi[0].style.color = "red";
+                    weightStatus.style.color = "red";
+                    optimumBodyWeightPlusMinusSpan.style.color = "red";
+                } else if(bmiResultP1 > 40) {
+                    weightStatus.innerHTML = "Obese (3rd degree)";
+                    bmi[0].style.color = "darkred";
+                    weightStatus.style.color = "darkred";
+                    optimumBodyWeightPlusMinusSpan.style.color = "darkred";
+                };
             }
             if(18.49 < bmiP1 && bmiP1 <25) {
                 const weightP1 = weightOfPerson(bmiP1, person1[1].value);
@@ -159,7 +152,7 @@ calc.onclick = ()=> {
                 const weightDifferenceOptimumMinus = diferenta(optimumWeightPerson, weightP1);
                 
                 weightStatus.innerHTML = "Normal weight";
-                normalBodyWeight.innerHTML = "Normal body weight is between " + "<span class='normalBodyWeightSpan'>" + weightNormalLow + "</span>" + " kg - " + "<span class='normalBodyWeightSpan'>" + weightNormalHigh + "</span>" + " kg";
+                normalBodyWeight.innerHTML = "Normal body weight: " + "<span class='normalBodyWeightSpan'>" + weightNormalLow + "</span>" + " kg - " + "<span class='normalBodyWeightSpan'>" + weightNormalHigh + "</span>" + " kg";
                 optimumBodyWeight.innerHTML = "Optimum weight: " + "<span id='optimumBodyWeightSpan'>" + optimumWeightPerson + "</span>" + " kg";
                 weightPlusMinus.innerHTML = "Your weight is in normal standards";
                 if(person1[2].value > optimumWeightPerson) {
@@ -189,7 +182,7 @@ calc.onclick = ()=> {
                 console.log(weightDifferenceOptimumMinus);
                 
                 weightStatus.innerHTML = "Underweight";
-                normalBodyWeight.innerHTML = "Normal body weight is between " + "<span class='normalBodyWeightSpan'>" + weightNormalHigh + "</span>" + " kg - " + "<span class='normalBodyWeightSpan'>" + weightNormalLow + "</span>" + " kg";
+                normalBodyWeight.innerHTML = "Normal body weight: " + "<span class='normalBodyWeightSpan'>" + weightNormalHigh + "</span>" + " kg - " + "<span class='normalBodyWeightSpan'>" + weightNormalLow + "</span>" + " kg";
                 optimumBodyWeight.innerHTML = "Optimum weight: " + "<span id='optimumBodyWeightSpan'>" + optimumWeightPerson + "</span>" + " kg";
                 weightPlusMinus.innerHTML = "For normal weight, gain between " + weightDifferenceMinusHigh + " kg - " + weightDifferenceMinusLow + " kg";
                 optimumBodyWeightPlusMinus.innerHTML = "For optimum weight, gain " + "<span id='optimumBodyWeightPlusMinusSpan'>" + weightDifferenceOptimumMinus + "<span>" + " kg";
