e0e21c758a52fb022a6b0636513a391dab582991
vlladd112 <vlladd112@gmail.com>
2018-09-24 01:49:43 +0300

    functionality: 90%

--- Weather API/models/weather.js
+++ Weather API/models/weather.js
@@ -9,104 +9,8 @@ class Weather{
         this.icon = "";
     }
 
-    WeatherFetchData() { 
-//        return $.ajax('"https://api.darksky.net/forecast/eb73a945bfe78f7a9e5a2ef08398b860/' + lonlat + '?lang=ro&units=si"', {
-        return $.ajax('https://api.darksky.net/forecast/eb73a945bfe78f7a9e5a2ef08398b860/23.6,46.76667?lang=ro&units=si', {
-            method: 'GET',
-            success: (weatherData)=> {
-                const weather = weatherData;
-                console.log(weather);
-                this.time = weatherData.currently.time;
-                this.temperature = weatherData.currently.temperature;
-                this.sumarry = weatherData.currently.summary;
-                this.humidity = weatherData.currently.humidity;
-                this.precipProbability = weatherData.currently.precipProbability;
-                this.pressure = weatherData.currently.pressure;
-                this.icon = weatherData.currently.icon;
-            },
-            error: ()=> {
-                console.log('nu merge!');
-            }
-        });
-    }
-     WeatherFetchDataBuc() { 
-        return $.ajax('https://api.darksky.net/forecast/eb73a945bfe78f7a9e5a2ef08398b860/26.10626,44.432251?lang=ro&units=si', {
-            method: 'GET',
-            success: (weatherData)=> {
-                const weather = weatherData;
-                console.log(weather);
-                this.time = weatherData.currently.time;
-                this.temperature = weatherData.currently.temperature;
-                this.sumarry = weatherData.currently.summary;
-                this.humidity = weatherData.currently.humidity;
-                this.precipProbability = weatherData.currently.precipProbability;
-                this.pressure = weatherData.currently.pressure;
-                this.icon = weatherData.currently.icon;
-            },
-            error: ()=> {
-                console.log('nu merge!');
-            }
-        });
-    }
-    WeatherFetchDataClu() { 
-        return $.ajax('https://api.darksky.net/forecast/eb73a945bfe78f7a9e5a2ef08398b860/23.6,46.76667?lang=ro&units=si', {
-            method: 'GET',
-            success: (weatherData)=> {
-                const weather = weatherData;
-                console.log(weather);
-                this.time = weatherData.currently.time;
-                this.temperature = weatherData.currently.temperature;
-                this.sumarry = weatherData.currently.summary;
-                this.humidity = weatherData.currently.humidity;
-                this.precipProbability = weatherData.currently.precipProbability;
-                this.pressure = weatherData.currently.pressure;
-                this.icon = weatherData.currently.icon;
-            },
-            error: ()=> {
-                console.log('nu merge!');
-            }
-        });
-    }
-    WeatherFetchDataCon() { 
-        return $.ajax('https://api.darksky.net/forecast/eb73a945bfe78f7a9e5a2ef08398b860/28.65,44.183331?lang=ro&units=si', {
-            method: 'GET',
-            success: (weatherData)=> {
-                const weather = weatherData;
-                console.log(weather);
-                this.time = weatherData.currently.time;
-                this.temperature = weatherData.currently.temperature;
-                this.sumarry = weatherData.currently.summary;
-                this.humidity = weatherData.currently.humidity;
-                this.precipProbability = weatherData.currently.precipProbability;
-                this.pressure = weatherData.currently.pressure;
-                this.icon = weatherData.currently.icon;
-            },
-            error: ()=> {
-                console.log('nu merge!');
-            }
-        });
-    }
-    WeatherFetchDataIas() { 
-        return $.ajax('https://api.darksky.net/forecast/eb73a945bfe78f7a9e5a2ef08398b860/27.6,47.166672?lang=ro&units=si', {
-            method: 'GET',
-            success: (weatherData)=> {
-                const weather = weatherData;
-                console.log(weather);
-                this.time = weatherData.currently.time;
-                this.temperature = weatherData.currently.temperature;
-                this.sumarry = weatherData.currently.summary;
-                this.humidity = weatherData.currently.humidity;
-                this.precipProbability = weatherData.currently.precipProbability;
-                this.pressure = weatherData.currently.pressure;
-                this.icon = weatherData.currently.icon;
-            },
-            error: ()=> {
-                console.log('nu merge!');
-            }
-        });
-    }
-    WeatherFetchDataTim() { 
-        return $.ajax('https://api.darksky.net/forecast/eb73a945bfe78f7a9e5a2ef08398b860/21.227221,45.749439?lang=ro&units=si', {
+    WeatherFetchData(lon, lat, lang, unit) { 
+        return $.ajax('https://api.darksky.net/forecast/eb73a945bfe78f7a9e5a2ef08398b860/' + lon + ',' + lat + '?lang=' + lang + '&units=' + unit, {
             method: 'GET',
             success: (weatherData)=> {
                 const weather = weatherData;
@@ -125,13 +29,3 @@ class Weather{
         });
     }
 };
-
-
-//const Weather = new Weather;
-//WeatherFetchData();
-//window.onload = ()=> {
-//    const currentWeather = new Weather;
-//    currentWeather.WeatherFetchData().then(()=>{
-//        console.log(currentWeather.icon);
-//    });
-//}
--- Weather API/views/displayWeather.js
+++ Weather API/views/displayWeather.js
@@ -1,9 +1,11 @@
 window.onload = ()=> {
     const currentWeather = new Weather;
 //    const lonlat = '23.6,46.76667';
-    currentWeather.WeatherFetchData().then(()=>{
-        console.log(currentWeather.icon);
-//        console.log(currentWeather.length);
+    const lon = '26.10626';
+    const lat = '44.432251';
+    let lang = 'ro';
+    let unit = 'si';
+    currentWeather.WeatherFetchData(lon, lat, lang, unit).then(()=>{
         const time = document.getElementById('timeSpan');
         const temperature = document.getElementById('temperatureSpan');
         const sumarry = document.getElementById('sumarrySpan');
@@ -19,37 +21,89 @@ window.onload = ()=> {
             var s = "0" + dt.getSeconds();
             return hr+ ':' + m.substr(-2) + ':' + s.substr(-2);  
         }
-         console.log(Unix_timestamp(1537454200));
+//         console.log(Unix_timestamp(1537454200));
         
         const transformTime = Unix_timestamp(currentWeather.time);
-        console.log(transformTime);
+//        console.log(transformTime);
         
-        time.innerHTML = transformTime;
-        temperature.innerHTML = currentWeather.temperature;
-        sumarry.innerHTML = currentWeather.sumarry;
-        humidity.innerHTML = currentWeather.humidity;
-        precipProbability.innerHTML = currentWeather.precipProbability;
-        pressure.innerHTML = currentWeather.pressure;
-        icon.innerHTML = currentWeather.icon;
+//        time.innerHTML = transformTime;
+//        temperature.innerHTML = currentWeather.temperature;
+//        sumarry.innerHTML = currentWeather.sumarry;
+//        humidity.innerHTML = currentWeather.humidity;
+//        precipProbability.innerHTML = currentWeather.precipProbability;
+//        pressure.innerHTML = currentWeather.pressure;
+//        icon.innerHTML = currentWeather.icon;
         
         const selectCity = document.getElementById('selectCity');
         console.log(selectCity.value);
         
         const submit = document.getElementById('submit');
         submit.addEventListener('click', ()=> {
+            const roLangValue = document.getElementById('roLangRadioBtn');
+            const enLangValue = document.getElementById('enLangRadioBtn');
+            const siUnitValue = document.getElementById('siUnitsRadioBtn');
+            const usUnitValue = document.getElementById('usUnitsRadioBtn');
+            const langCheck = ()=>{
+                if(roLangValue.checked === true) {
+                    let lang = roLangValue.value;
+//                    console.log("limba romana");
+                    return lang;
+                }
+                else if(enLangValue.checked === true) {
+                    let lang = enLangValue.value;
+//                    console.log("limba engleza");
+                    return lang;
+                };
+                
+                console.log('TTTTTTTTTTT')
+                console.log('asta returneaza functia defapt', lang);
+                return lang;
+                
+            };
+            
+            
+            lang = langCheck();
+            
+            
+            const unitsCheck = ()=>{
+                if(siUnitValue.checked === true) {
+                    let unit = siUnitValue.value;
+//                    console.log("SI SI SI");
+                    return unit;
+                }
+                else if(usUnitValue.checked === true) {
+                    let unit = usUnitValue.value;
+//                    console.log("imperial");
+                    return unit;
+                };
+                return unit;
+            }
+            
+            
+            unit = unitsCheck();
+            
+            
+            console.log('langcheck language:', lang)
+            console.log('langcheck unit:', unit)
             console.log(selectCity.value);
             if(selectCity.value === "Bucuresti") {
-                currentWeather.WeatherFetchDataBuc().then(()=>{
+                const lon = '26.10626';
+                const lat = '44.432251';
+                currentWeather.WeatherFetchData(lon, lat, lang, unit).then(()=>{
                     time.innerHTML = transformTime;
                     temperature.innerHTML = currentWeather.temperature;
                     sumarry.innerHTML = currentWeather.sumarry;
                     humidity.innerHTML = currentWeather.humidity;
                     precipProbability.innerHTML = currentWeather.precipProbability;
                     pressure.innerHTML = currentWeather.pressure;
-                    icon.innerHTML = currentWeather.icon;   
+                    icon.innerHTML = currentWeather.icon;
+                    console.log(currentWeather.sumarry);
                 })
+                
             } else if(selectCity.value === "Cluj-Napoca") {
-                currentWeather.WeatherFetchDataClu().then(()=>{
+                const lon = '23.6';
+                const lat = '46.76667';
+                currentWeather.WeatherFetchData(lon, lat, lang, unit).then(()=>{
                     time.innerHTML = transformTime;
                     temperature.innerHTML = currentWeather.temperature;
                     sumarry.innerHTML = currentWeather.sumarry;
@@ -59,7 +113,9 @@ window.onload = ()=> {
                     icon.innerHTML = currentWeather.icon;   
                 })
             } else if(selectCity.value === "Constanta") {
-                currentWeather.WeatherFetchDataCon().then(()=>{
+                const lon = '28.65';
+                const lat = '44.183331';
+                currentWeather.WeatherFetchData(lon, lat, lang, unit).then(()=>{
                     time.innerHTML = transformTime;
                     temperature.innerHTML = currentWeather.temperature;
                     sumarry.innerHTML = currentWeather.sumarry;
@@ -69,7 +125,9 @@ window.onload = ()=> {
                     icon.innerHTML = currentWeather.icon;   
                 })
             } else if(selectCity.value === "Iasi") {
-                currentWeather.WeatherFetchDataIas().then(()=>{
+                const lon = '27.6';
+                const lat = '47.166672';
+                currentWeather.WeatherFetchData(lon, lat, lang, unit).then(()=>{
                     time.innerHTML = transformTime;
                     temperature.innerHTML = currentWeather.temperature;
                     sumarry.innerHTML = currentWeather.sumarry;
@@ -79,7 +137,9 @@ window.onload = ()=> {
                     icon.innerHTML = currentWeather.icon;   
                 })
             } else if(selectCity.value === "Timisoara") {
-                currentWeather.WeatherFetchDataTim().then(()=>{
+                const lon = '21.227221';
+                const lat = '45.749439';
+                currentWeather.WeatherFetchData(lon, lat, lang, unit).then(()=>{
                     time.innerHTML = transformTime;
                     temperature.innerHTML = currentWeather.temperature;
                     sumarry.innerHTML = currentWeather.sumarry;
