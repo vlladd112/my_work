2d67ae7ba8a52bb0676d08fc98b341862a8ee6b4
vlladd112 <vlladd112@gmail.com>
2018-10-27 13:15:14 +0300

    lotto 6-49 funtionslity complete

--- Lotto 6-49/scriptFinal.js
+++ Lotto 6-49/scriptFinal.js
@@ -9,14 +9,26 @@ generateNumbers.addEventListener('click', ()=>{
     for (i=1; i<=upToLottoNr; i++) {
         numbersArray.push(i);
         randomNr = numbersArray.length;
+        result.innerHTML = "";
     }
     for (let i=amountOfNumbersToBeDisplayed; i>0; i--) {
         let rn = Math.ceil(Math.random()*randomNr)-1;
         let numarDePushat = numbersArray.splice(rn,1);
-        finalNumbers.push(numarDePushat);
         randomNr -=1;
+        const numarSimplu = String(numarDePushat);
+        finalNumbers.push(numarSimplu);
+        const divForShadow = document.createElement('div');
+        divForShadow.className = "divForShadow";
+        result.appendChild(divForShadow);
+        const divForNumberSpan = document.createElement('div');
+        divForNumberSpan.className = "divForNumberSpan";
+        divForShadow.appendChild(divForNumberSpan);
+        const createSpanForEachNumber = document.createElement('span');
+        createSpanForEachNumber.innerHTML = numarSimplu;
+        divForNumberSpan.appendChild(createSpanForEachNumber);
     }
-    const numereleFinale = String(finalNumbers);
-    const noCommas = numereleFinale.replace(/,/g, " &nbsp;&nbsp;");
-    result.innerHTML = noCommas;
+//    const numereleFinale = String(finalNumbers);
+//    const noCommas = finalNumbers.replace(/,/g, " &nbsp;&nbsp;");
+//    console.log(finalNumbers);
+//    result.innerHTML = noCommas;
 })
