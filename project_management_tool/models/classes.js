9819642dfab35ed63c5cc1a0a921cb9081a6f431
vlladd112 <vlladd112@gmail.com>
2019-01-18 10:02:28 +0200

    absolute last commit

--- project_management_tool/models/classes.js
+++ project_management_tool/models/classes.js
@@ -1,7 +1,7 @@
 function Sprint(id, name){
     this.id = id;
     this.name = name;
-}
+};
 
 function Issue(id, type, name, sprint, createdBy, asignee, description, status, task, comments, updatedAt, createdAt){
     this.id = id;
@@ -16,16 +16,4 @@ function Issue(id, type, name, sprint, createdBy, asignee, description, status, 
     this.comments = comments;
     this.updatedAt = updatedAt;
     this.createdAt = createdAt;
-}
-
-function updateIssue(type, name, sprint, asignee, description, status, task, comments, updatedAt){
-    this.type = type;
-    this.name = name;
-    this.sprint = sprint;
-    this.asignee = asignee;
-    this.description = description;
-    this.status = status;
-    this.task = task;
-    this.comments = comments;
-    this.updatedAt = updatedAt;
-}
+};
